988e0feab1842b926c4518c831b062423f4969ec
jahangir khedri <khedri.jahangir@gmail.com>
2022-12-29 13:21:02 +0330

    add form requests

--- app/Http/Controllers/Api/V1/LoginController.php
+++ app/Http/Controllers/Api/V1/LoginController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends BaseController
 {
-    public function login(Request $request)
+    public function login(LoginFormRequest $request)
     {
 
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
--- app/Http/Controllers/Api/V1/RegisterController.php
+++ app/Http/Controllers/Api/V1/RegisterController.php
@@ -2,36 +2,14 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterFormRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Validator;
+
 
 class RegisterController extends BaseController
 {
-    public function register(Request $request)
-
+    public function register(RegisterFormRequest $request)
     {
-
-        $validator = Validator::make($request->all(), [
-
-            'name' => 'required',
-
-            'email' => 'required|email',
-
-            'password' => 'required',
-
-            'password_confirmation' => 'required|same:password',
-
-        ]);
-
-
-        if ($validator->fails()) {
-
-            return $this->sendError('Validation Error.', $validator->errors());
-
-        }
-
 
         $input = $request->all();
 
